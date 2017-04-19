d040b0cb23fc177f726c7bd07ad88122c485252a
Andrew Cutler <macropin@gmail.com>
2017-04-19 16:50:15 +1000

    fix watermark and space handling

--- commands/ffmpeg.sh
+++ commands/ffmpeg.sh
@@ -9,6 +9,7 @@ done < "${1:-/dev/stdin}"
 
 INPUT_FILE="${INPUT_PATH}/$JOB"
 OUTPUT_FILE="${OUTPUT_PATH}/$JOB"
-WATERMARK=/media/watermark.png
 
-ffmpeg -i $INPUT_FILE -vf "movie=${WATERMARK} [watermark]; [in][watermark] overlay=(main_w-overlay_w-10):(main_h-overlay_h-10) [out]" ${OUTPUT_FILE}
+: ${WATERMARK:='/media/watermark.png'}
+
+ffmpeg -i "${INPUT_FILE}" -vf "movie=${WATERMARK} [watermark]; [in][watermark] overlay=(main_w-overlay_w-10):(main_h-overlay_h-10) [out]" "${OUTPUT_FILE}"
